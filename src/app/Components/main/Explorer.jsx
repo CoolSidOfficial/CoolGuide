c3bb8ea1e6947775411ea5805d0e4d6b87cab593
Coolsidofficial <hackersiddhant1@gmail.com>
2024-09-10 13:05:42 +0530

    added search features

--- src/app/Components/main/Explorer.jsx
+++ src/app/Components/main/Explorer.jsx
@@ -7,18 +7,18 @@ function Explorer() {
         <div className="text-white p-3 text-xl">Browse our comprehensive guides, search for products, and<br/> connect with other users to make an informed decision.</div>
        <div>
 
-        <input className="bg-black text-white border-white w-80  h-8" type="text" placeholder="Search For Electronics.."/>
-        <button className="w-36 bg-white ">Search</button>
+        <input className="bg-black text-white border-white w-96 p-2  rounded  h-8" type="text" placeholder="   Search For Electronics.."/>
+        <button className="h-8 ml-2 bg-white w-24 rounded-lg ">Search</button>
        </div>
        
        
         <div className="grid mt-3 grid-rows-2 grid-flow-col  gap-4 ">
-        <button className="bg-white rounded  hover:bg-slate-300 w-36 h-11">Laptops</button>
-        <button className="bg-white rounded hover:bg-slate-300  w-36">Smartphones</button>
-        <button className="bg-white  rounded hover:bg-slate-300 w-36">TVs</button>
-        <button className="bg-white rounded   hover:bg-slate-300 w-36">Headphones</button>
-        <button className="bg-white  rounded  hover:bg-slate-300 w-36">Fridge</button>
-        <button className="bg-white rounded hover:bg-slate-300 w-36">ACs</button>
+        <button className="bg-white rounded-md  hover:bg-slate-300 w-36 h-11">Laptops</button>
+        <button className="bg-white rounded-md hover:bg-slate-300  w-36">Smartphones</button>
+        <button className="bg-white  rounded-md hover:bg-slate-300 w-36">TVs</button>
+        <button className="bg-white rounded-md   hover:bg-slate-300 w-36">Headphones</button>
+        <button className="bg-white  rounded-md  hover:bg-slate-300 w-36">Fridge</button>
+        <button className="bg-white rounded-md hover:bg-slate-300 w-36">ACs</button>
         </div>
 
 
---
+++ src/app/Components/main/Featured.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Featured() {
+  return (
+    <>
+    <div className='flex'>
+    <div className=''>Featured Products</div>
+    <div className=''>Check Out Our Featured Electronics</div>
+    <div className=''>Explore our hand-picked selection of the latest and greatest electronics, curated by our team of experts.</div>
+    <div>
+        <div >
+            <span></span>
+            <span></span>
+            <span></span> 
+            <span></span>
+            </div>
+        <div>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+
+        </div>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <div></div>
+
+    </div>
+
+    </div>
+    
+    </>
+  )
+}
+
+export default Featured
